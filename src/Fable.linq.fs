14eb6f52bac762e611b108cd31955510ac5e8eef
wangweipeng <weipeng.wang@gllue.com>
2018-12-23 20:07:14 +0800

    update

--- src/Fable.linq.fs
+++ src/Fable.linq.fs
@@ -1,6 +1,6 @@
 module Fable.Linq.Main
 
-type QueryBuilder() = 
+type FableQueryBuilder() = 
     member x.For(tz:List<'T>, f:'T -> 'T) : List<'T> = 
         List.map (f) tz
     member x.Yield(v:'T) : 'T = 
@@ -13,6 +13,17 @@ type QueryBuilder() =
     [<CustomOperation("select", MaintainsVariableSpace=true)>]
     member x.Select ( source:List<'T>, [<ProjectionParameter>] f:'T -> 'T2 ) : List<'T2> = 
        List.map (fun a -> f(a)) source 
-let fablequery = QueryBuilder()
+
+    [<CustomOperation("sortBy", MaintainsVariableSpace=true)>]
+    member x.SortBy ( source:List<'T>, [<ProjectionParameter>] f:'T -> 'Key) : List<'T> = 
+       List.sortBy (fun a -> f(a)) source 
+    
+    [<CustomOperation("thenBy", MaintainsVariableSpace=true)>]
+    member x.ThenBy ( source:List<'T>, [<ProjectionParameter>] f:'T -> 'Key ) : List<'T> = 
+       List.sortByDescending (fun a -> f(a)) source 
+
+//'sortBy', 'thenBy', 'groupBy', 'groupValBy', 
+//'join', 'groupJoin', 'sumBy' and 'averageBy
+let fablequery = FableQueryBuilder()
 
 

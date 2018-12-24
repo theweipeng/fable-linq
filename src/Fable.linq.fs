6a279c4874d72563978366b825961a2f29466c7b
bigteech <pythonvoid@outlook.com>
2018-12-24 18:13:33 +0800

    add some

--- src/Fable.linq.fs
+++ src/Fable.linq.fs
@@ -85,10 +85,8 @@ type FableQueryBuilder() =
 
    // member this.GroupJoin : QuerySource<'Outer,'Q> * QuerySource<'Inner,'Q> * ('Outer -> 'Key) * ('Inner -> 'Key) * ('Outer -> seq<'Inner> -> 'Result) -> QuerySource<'Result,'Q> = jsNative
    // member this.GroupValBy : List<'T> * ('T -> 'Value) * ('T -> 'Key) -> QuerySource<IGrouping<'Key,'Value>,'Q> = jsNative
-   // member this.Head : List<'T> -> 'T = jsNative
    // member this.HeadOrDefault : List<'T> -> 'T = jsNative
    // member this.Join : QuerySource<'Outer,'Q> * QuerySource<'Inner,'Q> * ('Outer -> 'Key) * ('Inner -> 'Key) * ('Outer -> 'Inner -> 'Result) -> QuerySource<'Result,'Q> = jsNative
-   // member this.Last : List<'T> -> 'T = jsNative
    // member this.LastOrDefault : List<'T> -> 'T = jsNative
    // member this.LeftOuterJoin : QuerySource<'Outer,'Q> * QuerySource<'Inner,'Q> * ('Outer -> 'Key) * ('Inner -> 'Key) * ('Outer -> seq<'Inner> -> 'Result) -> QuerySource<'Result,'Q> = jsNative
    // member this.MaxBy : List<'T> * ('T -> 'Value) -> 'Value = jsNative
@@ -98,28 +96,44 @@ type FableQueryBuilder() =
    // member this.Nth : List<'T> * int -> 'T = jsNative
    // member this.Quote : Expr<'T> -> Expr<'T> = jsNative
    // member this.Run : Expr<QuerySource<'T,IQueryable>> -> IQueryable<'T> = jsNative
-   // member this.Select : List<'T> * ('T -> 'Result) -> QuerySource<'Result,'Q> = jsNative
-   // member this.Skip : List<'T> * int -> List<'T> = jsNative
-   // member this.SkipWhile : List<'T> * ('T -> bool) -> List<'T> = jsNative
-   // member this.SortBy : List<'T> * ('T -> 'Key) -> List<'T> = jsNative
-   // member this.SortByDescending : List<'T> * ('T -> 'Key) -> List<'T> = jsNative
    // member this.SortByNullable : List<'T> * ('T -> Nullable<'Key>) -> List<'T> = jsNative
    // member this.SortByNullableDescending : List<'T> * ('T -> Nullable<'Key>) -> List<'T> = jsNative
    // member this.Source : IEnumerable<'T> -> QuerySource<'T,IEnumerable> = jsNative
    // member this.Source : IQueryable<'T> -> List<'T> = jsNative
    // member this.SumBy : List<'T> * ('T -> ^Value) -> ^Value = jsNative
    // member this.SumByNullable : List<'T> * ('T -> Nullable<^Value>) -> Nullable<^Value> = jsNative
-   // member this.Take : List<'T> * int -> List<'T> = jsNative
-   // member this.TakeWhile : List<'T> * ('T -> bool) -> List<'T> = jsNative
-   // member this.ThenBy : List<'T> * ('T -> 'Key) -> List<'T> = jsNative
-   // member this.ThenByDescending : List<'T> * ('T -> 'Key) -> List<'T> = jsNative
    // member this.ThenByNullable : List<'T> * ('T -> Nullable<'Key>) -> List<'T> = jsNative
    // member this.ThenByNullableDescending : List<'T> * ('T -> Nullable<'Key>) -> List<'T> = jsNative
-   // member this.Where : List<'T> * ('T -> bool) -> List<'T> = jsNative
    // member this.Yield : 'T -> List<'T> = jsNative
    // member this.YieldFrom : List<'T> -> List<'T> = jsNative
-   // member this.Zero : unit -> List<'T> = jsNative
 
+   [<CustomOperation("head", MaintainsVariableSpace=true)>]
+   member x.Head ()  = 
+      List.head
+
+   [<CustomOperation("last", MaintainsVariableSpace=true)>]
+   member x.Last ()  = 
+      List.last
+
+   [<CustomOperation("skip", MaintainsVariableSpace=true)>]
+   member x.Skip (v:int)  = 
+      List.skip v
+   
+   [<CustomOperation("skipWhile", MaintainsVariableSpace=true)>]
+   member x.SkipWhile (source:List<'T>, f:'T -> bool)  = 
+      List.skipWhile f source
+
+   [<CustomOperation("take", MaintainsVariableSpace=true)>]
+   member x.Take (v:int)  = 
+      List.take v
+
+   [<CustomOperation("takeWhile", MaintainsVariableSpace=true)>]
+   member x.TakeWhile (source:List<'T>, f:'T -> bool)  = 
+      List.takeWhile f source
+
+   [<CustomOperation("zero", MaintainsVariableSpace=true)>]
+   member x.Zero ()  = 
+      List.empty
 
 let fablequery = FableQueryBuilder()
 
